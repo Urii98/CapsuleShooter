9bf403d9aa5c740ac5e02c55d3289775a304ed97
Urii98 <99950184+Urii98@users.noreply.github.com>
2024-11-11 21:03:20 +0100

    Cleaning

--- Assets/Scripts/UDP/Client.cs
+++ Assets/Scripts/UDP/Client.cs
@@ -4,7 +4,6 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using UnityEngine;
-//using UnityEngine.tvOS;
 
 public class Client : MonoBehaviour
 {
--- Assets/Scripts/UDP/Server.cs
+++ Assets/Scripts/UDP/Server.cs
@@ -5,7 +5,6 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using UnityEngine;
-//using UnityEngine.tvOS;
 
 public class ConnectedClient
 {
