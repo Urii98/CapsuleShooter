989508b07c5cb9ad6693d8df23a875e64d3e2fa2
NachoMoren <79315360+NachoMoren@users.noreply.github.com>
2025-01-08 22:21:01 +0100

    Cambiados server y cliente a los de antes del merge

--- Assets/Scripts/UDP/Server.cs
+++ Assets/Scripts/UDP/Server.cs
@@ -5,7 +5,8 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using UnityEngine;
-using System.Collections.Concurrent; // Para ConcurrentQueue
+using System.Collections.Concurrent;
+using System.Linq; // Para ConcurrentQueue
 
 [Serializable]
 public struct HealData
@@ -37,7 +38,12 @@ public class Server : MonoBehaviour, Networking
     private Dictionary<int, HealData> activeHeals = new Dictionary<int, HealData>();
     private int nextHealId = 0;
 
-    
+    // ACK management
+    private Dictionary<EndPoint,int> clientSequenceIDs = new Dictionary<EndPoint, int>();
+    private Dictionary<int, (byte[], EndPoint)> unacknowledgedPackets = new Dictionary<int, (byte[], EndPoint)>();
+    private Dictionary<int, DateTime> packetTimestamps = new Dictionary<int, DateTime>(); 
+    private float ackTimeout = 1.0f; 
+
     private ConcurrentQueue<int> healsToRemove = new ConcurrentQueue<int>();
 
     private void Start()
@@ -75,7 +81,20 @@ public class Server : MonoBehaviour, Networking
 
         Debug.Log($"Server received message from {fromAddress}: {message}");
 
-        if (message.StartsWith("ClientConnected:"))
+        if (message.StartsWith("ACK:"))
+        {
+            int ackId = int.Parse(message.Substring("ACK:".Length));
+            if (unacknowledgedPackets.TryGetValue(ackId, out var packetInfo))
+            {
+                if (Equals(packetInfo.Item2, fromAddress)) 
+                {
+                    unacknowledgedPackets.Remove(ackId);
+                    packetTimestamps.Remove(ackId);
+                    Debug.Log($"Acknowledged packet with sequenceId: {ackId} from {fromAddress}");
+                }
+            }
+        }
+        else if (message.StartsWith("ClientConnected:"))
         {
             string[] parts = message.Split(':');
             string playerId = parts[1];
@@ -100,9 +119,12 @@ public class Server : MonoBehaviour, Networking
                     Debug.Log($"Added new client: {playerId} from {fromAddress}");
                 }
             }
-            byte[] data = Encoding.ASCII.GetBytes("ServerConnected");
-            SendPacket(data, fromAddress);
-
+            clientSequenceIDs[fromAddress] = GetSequenceIDFromPacket(inputPacket);
+            string syncMessage = "SyncSeqID";
+            byte[] syncData = Encoding.ASCII.GetBytes(syncMessage);
+            SendPacket(syncData, fromAddress);
+            Debug.Log($"Initialized sequenceID for {fromAddress}");
+            SendAck(fromAddress);
         }
         else if (message.StartsWith("PlayerData:"))
         {
@@ -113,11 +135,17 @@ public class Server : MonoBehaviour, Networking
                     SendPacket(inputPacket, Length, SocketFlags.None, client.EndPoint);
                 }
             }
+            SendAck(fromAddress);
         }
         else if (message.StartsWith("HealPicked:"))
         {
 
             string json = message.Substring("HealPicked:".Length);
+            int indexOfColon = json.LastIndexOf(':');
+            if (indexOfColon != -1)
+            {
+                json = json.Substring(0, indexOfColon);
+            }
             HealData healPicked = JsonUtility.FromJson<HealData>(json);
 
             lock (connectedClients)
@@ -132,7 +160,23 @@ public class Server : MonoBehaviour, Networking
                 }
 
             }
+            SendAck(fromAddress);
         }
+
+    }
+    private int GetSequenceIDFromPacket(byte[] packet)
+    {
+        
+        int sequenceID = BitConverter.ToInt32(packet, packet.Length - sizeof(int));
+        return sequenceID;
+    }
+
+    private void SendAck(EndPoint toAddress)
+    {
+        string ackMessage = $"ACK:{clientSequenceIDs[toAddress]}";
+        byte[] ackData = Encoding.ASCII.GetBytes(ackMessage);
+        socket.SendTo(ackData, toAddress);
+        Debug.Log($"Sent ACK for sequenceId: {clientSequenceIDs[toAddress]} to {toAddress}");
     }
 
     public void OnUpdate()
@@ -151,12 +195,42 @@ public class Server : MonoBehaviour, Networking
 
     public void SendPacket(byte[] packet, EndPoint toAddress)
     {
-        socket.SendTo(packet, toAddress);
+        int sequenceID;
+        if (!clientSequenceIDs.TryGetValue(toAddress, out sequenceID))
+        {
+            sequenceID = 0; 
+        }
+        sequenceID++;
+        clientSequenceIDs[toAddress] = sequenceID;
+
+        byte[] packetWithId = packet.Concat(Encoding.ASCII.GetBytes($":{sequenceID}")).ToArray();
+
+        unacknowledgedPackets[sequenceID] = (packetWithId, toAddress);
+        packetTimestamps[sequenceID] = DateTime.Now;
+
+        socket.SendTo(packetWithId, toAddress);
+        
+        Debug.Log($"Sent packet with sequenceId: {sequenceID} and message: {Encoding.ASCII.GetString(packetWithId)} to {toAddress}");
     }
 
     public void SendPacket(byte[] packet, int length, SocketFlags socketFlags, EndPoint toAddress)
     {
-        socket.SendTo(packet, length, socketFlags, toAddress);
+        int sequenceID;
+        if (!clientSequenceIDs.TryGetValue(toAddress, out sequenceID))
+        {
+            sequenceID = 0; 
+        }
+        sequenceID++;
+        clientSequenceIDs[toAddress] = sequenceID;
+
+        byte[] packetWithId = packet.Concat(Encoding.ASCII.GetBytes($":{sequenceID}")).ToArray(); 
+
+        unacknowledgedPackets[sequenceID] = (packetWithId, toAddress);
+        packetTimestamps[sequenceID] = DateTime.Now;
+
+        socket.SendTo(packetWithId, length, socketFlags, toAddress);
+
+        Debug.Log($"Sent packet with sequenceId: {sequenceID} and message: {Encoding.ASCII.GetString(packetWithId)} to {toAddress}");
     }
 
     public void OnDisconnect()
@@ -185,6 +259,26 @@ public class Server : MonoBehaviour, Networking
     private void Update()
     {
         OnUpdate();
+
+        //List<int> packetsToRetransmit = new List<int>();
+
+        //foreach (var pair in packetTimestamps)
+        //{
+        //    if ((DateTime.Now - pair.Value).TotalSeconds > ackTimeout)
+        //    {
+        //        packetsToRetransmit.Add(pair.Key);
+        //    }
+        //}
+
+        //foreach (int seqId in packetsToRetransmit)
+        //{
+        //    if (unacknowledgedPackets.TryGetValue(seqId, out (byte[] packet, EndPoint toAddress) entry))
+        //    {
+        //        socket.SendTo(entry.packet, entry.toAddress);
+        //        packetTimestamps[seqId] = DateTime.Now;
+        //        Debug.Log($"Retransmitting packet with sequenceId: {seqId} to {entry.toAddress}");   
+        //    }
+        //}
     }
 
     private void ListenForClients()
