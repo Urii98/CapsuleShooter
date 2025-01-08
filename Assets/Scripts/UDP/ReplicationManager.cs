2d0463b95246223bd2e22d385d555b19d3d50424
NachoMoren <79315360+NachoMoren@users.noreply.github.com>
2025-01-08 21:52:59 +0100

    Funciona Acknowledgements, pero no retransmite los mensajes que no se envian

--- Assets/Scripts/UDP/ReplicationManager.cs
+++ Assets/Scripts/UDP/ReplicationManager.cs
@@ -10,19 +10,25 @@ public class ReplicationManager : MonoBehaviour
     public byte[] ToBytes(PlayerState player)
     {
         string json = "PlayerData:" + JsonUtility.ToJson(player);
-        Debug.Log($"Sending JSON: {json}");
+        //Debug.Log($"Sending JSON: {json}");
         return Encoding.ASCII.GetBytes(json);
     }
 
     public PlayerState FromBytes(byte[] data, int size)
     {
         string json = Encoding.ASCII.GetString(data, 0, size);
-        Debug.Log($"Received JSON: {json}");
+        //Debug.Log($"Received JSON: {json}");
         if (json.StartsWith("PlayerData:"))
         {
             json = json.Substring("PlayerData:".Length);
-        }
 
+            int indexOfColon = json.LastIndexOf(':');
+            if (indexOfColon != -1)
+            {
+                json = json.Substring(0, indexOfColon); 
+            }
+        }
+        //Debug.Log($"Corrected JSON: {json}");
         try
         {
             return JsonUtility.FromJson<PlayerState>(json);
